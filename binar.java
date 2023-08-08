0915527f673908a59aae0d9f710cfab39d8e20b4
Prajjwal sharma <prajjwalbh25@gmail.com>
2023-08-08 23:43:16 +0530

    binary search using recursion algorithm.

---
+++ binar.java
@@ -0,0 +1,24 @@
+//import static jdk.internal.org.jline.utils.Colors.s;
+
+public class binar {
+    public static void main(String[] args) {
+int[] arr={2,34,54,56,78};
+int target = 56;
+int s =0;
+int e=arr.length-1;
+        System.out.println(binu(arr,target,s,e));
+    }
+    static int binu(int[] arr,int target,int s,int e){
+        if(s>e){
+            return -1;
+        }
+        int m = s+(e-s)/2;
+        if(arr[m]==target){
+            return m;
+        }
+        if(target<arr[m]){
+            return binu(arr,target,s,m-1);
+        }
+        return binu(arr,target,m+1,e);
+    }
+}
